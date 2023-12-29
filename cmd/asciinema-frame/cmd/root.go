394510a2fefd1446f96ac0adcf26361d44a6f0fc
rsteube <rsteube@users.noreply.github.com>
2023-12-29 16:19:41 +0100

    fix gitignore

---
+++ cmd/asciinema-frame/cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	frame "github.com/rsteube/asciinema-frame"
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "asciinema-frame [file] [time]",
+	Short: "",
+	Args:  cobra.ExactArgs(2),
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		file, err := os.Open(args[0])
+		if err != nil {
+			return err
+		}
+
+		time, err := strconv.ParseFloat(args[1], 64)
+		if err != nil {
+			return err
+		}
+
+		f := frame.Frame(file, time)
+
+		switch {
+		case cmd.Flag("poster").Changed:
+			fmt.Fprintln(cmd.OutOrStdout(), f.Poster())
+		default:
+			fmt.Fprintln(cmd.OutOrStdout(), f.RawString())
+		}
+
+		return nil
+	},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	rootCmd.Flags().Bool("poster", false, "asciinema poster")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
---
+++ cmd/asciinema-frame/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/asciinema-frame/cmd/asciinema-frame/cmd"
+
+func main() {
+	cmd.Execute()
+}
